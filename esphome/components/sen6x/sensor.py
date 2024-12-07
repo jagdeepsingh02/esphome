c69e21184c3c64fe1a314910463388e129b57065
Your Nameclear <you@example.com>
2024-12-07 22:32:04 +0000

    Progressing on setting sensor.py

---
+++ esphome/components/sen6x/sensor.py
@@ -0,0 +1,187 @@
+from esphome import automation
+import esphome.codegen as cg
+from esphome.components import i2c, sensor
+import esphome.config_validation as cv
+from esphome.const import (
+    CONF_HUMIDITY,
+    CONF_OFFSET,
+    CONF_PM_1_0,
+    CONF_PM_2_5,
+    CONF_PM_4_0,
+    CONF_PM_10_0,
+    CONF_STORE_BASELINE,
+    CONF_TEMPERATURE,
+    DEVICE_CLASS_AQI,
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_PM1,
+    DEVICE_CLASS_PM10,
+    DEVICE_CLASS_PM25,
+    DEVICE_CLASS_TEMPERATURE,
+    ICON_CHEMICAL_WEAPON,
+    ICON_RADIATOR,
+    ICON_THERMOMETER,
+    ICON_WATER_PERCENT,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_CELSIUS,
+    UNIT_MICROGRAMS_PER_CUBIC_METER,
+    UNIT_PERCENT,
+)
+
+CODEOWNERS = ["@jagdeepsingh02"]
+DEPENDENCIES = ["i2c"]
+# AUTO_LOAD = [""]
+
+sen6x_ns = cg.esphome_ns.namespace("sen6x")
+SEN6XComponent = sen6x_ns.class_("SEN6XComponent", cg.PollingComponent, i2c.I2CDevice)
+
+# Define enums Here
+RhtAccelerationMode = sen6x_ns.enum("RhtAccelerationMode")
+# Create a dictionary of the acceleration modes to be used in the configuration schema below
+ACCELERATION_MODES = {
+    "low": RhtAccelerationMode.LOW_ACCELERATION,
+    "medium": RhtAccelerationMode.MEDIUM_ACCELERATION,
+    "high": RhtAccelerationMode.HIGH_ACCELERATION,
+}
+# Create a dictionary of the acceleration modes to be used in the configuration schema below
+# and in the configuration validation function below
+# The dictionary key is the string that will be used in the configuration schema
+# The dictionary value is the enum value that will be used in the configuration validation function
+# The enum value is the value that will be used in the C++ code
+# The enum value is defined in the C++ code as follows:
+# enum class RhtAccelerationMode {
+#     LOW_ACCELERATION = 0,
+#     MEDIUM_ACCELERATION = 1,
+#     HIGH_ACCELERATION = 2,
+# };
+# The enum value is used in the C++ code as follows:
+# RhtAccelerationMode acceleration_mode = RhtAccelerationMode::LOW_ACCELERATION;
+
+
+# define Constants here
+CONF_ACCELERATION_MODE = "acceleration_mode"
+CONF_ALGORITHM_TUNING = "algorithm_tuning"
+CONF_AUTO_CLEANING_INTERVAL = "auto_cleaning_interval"
+CONF_GAIN_FACTOR = "gain_factor"
+CONF_GATING_MAX_DURATION_MINUTES = "gating_max_duration_minutes"
+CONF_INDEX_OFFSET = "index_offset"
+CONF_LEARNING_TIME_GAIN_HOURS = "learning_time_gain_hours"
+CONF_LEARNING_TIME_OFFSET_HOURS = "learning_time_offset_hours"
+CONF_NORMALIZED_OFFSET_SLOPE = "normalized_offset_slope"
+CONF_NOX = "nox"
+CONF_STD_INITIAL = "std_initial"
+CONF_TEMPERATURE_COMPENSATION = "temperature_compensation"
+CONF_TIME_CONSTANT = "time_constant"
+CONF_VOC = "voc"
+CONF_VOC_BASELINE = "voc_baseline"
+
+# define Actions here
+StartFanAction = sen6x_ns.class_("StartFanAction", automation.Action)
+
+
+# Functions to validate the configuration schema
+def float_previously_pct(value):
+    if isinstance(value, str) and "%" in value:
+        raise cv.Invalid(
+            f"The value '{value}' is a percentage. Suggested value: {float(value.strip('%')) / 100}"
+        )
+    return value
+
+
+# Define config schema for nested configuration
+ALGORITHM_TUNING_SCHEMA = cv.Schema(
+    {
+        cv.Optional(CONF_ALGORITHM_TUNING): cv.Schema(
+            {
+                cv.Optional(CONF_INDEX_OFFSET, default=100): cv.int_range(1, 250),
+                cv.Optional(CONF_LEARNING_TIME_OFFSET_HOURS, default=12): cv.int_range(
+                    1, 1000
+                ),
+                cv.Optional(CONF_LEARNING_TIME_GAIN_HOURS, default=12): cv.int_range(
+                    1, 1000
+                ),
+                cv.Optional(
+                    CONF_GATING_MAX_DURATION_MINUTES, default=720
+                ): cv.int_range(0, 3000),
+                cv.Optional(CONF_STD_INITIAL, default=50): cv.int_,
+                cv.Optional(CONF_GAIN_FACTOR, default=230): cv.int_range(1, 1000),
+            }
+        )
+    }
+)
+
+# define the configuration schema
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(SEN6XComponent),
+            cv.Optional(CONF_PM_1_0): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+                icon=ICON_CHEMICAL_WEAPON,
+                accuracy_decimals=2,
+                device_class=DEVICE_CLASS_PM1,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_PM_2_5): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+                icon=ICON_CHEMICAL_WEAPON,
+                accuracy_decimals=2,
+                device_class=DEVICE_CLASS_PM25,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_PM_4_0): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+                icon=ICON_CHEMICAL_WEAPON,
+                accuracy_decimals=2,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_PM_10_0): sensor.sensor_schema(
+                unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+                icon=ICON_CHEMICAL_WEAPON,
+                accuracy_decimals=2,
+                device_class=DEVICE_CLASS_PM10,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_AUTO_CLEANING_INTERVAL): cv.update_interval,
+            cv.Optional(CONF_VOC): sensor.sensor_schema(
+                icon=ICON_RADIATOR,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_AQI,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ).extend(ALGORITHM_TUNING_SCHEMA),
+            cv.Optional(CONF_NOX): sensor.sensor_schema(
+                icon=ICON_RADIATOR,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_AQI,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ).extend(ALGORITHM_TUNING_SCHEMA),
+            cv.Optional(CONF_STORE_BASELINE, default=True): cv.boolean,
+            cv.Optional(CONF_VOC_BASELINE): cv.hex_uint16_t,
+            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_CELSIUS,
+                icon=ICON_THERMOMETER,
+                accuracy_decimals=2,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PERCENT,
+                icon=ICON_WATER_PERCENT,
+                accuracy_decimals=2,
+                device_class=DEVICE_CLASS_HUMIDITY,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_TEMPERATURE_COMPENSATION): cv.Schema(
+                {
+                    cv.Optional(CONF_OFFSET, default=0): cv.float_,
+                    cv.Optional(CONF_NORMALIZED_OFFSET_SLOPE, default=0): cv.All(
+                        float_previously_pct, cv.float_
+                    ),
+                    cv.Optional(CONF_TIME_CONSTANT, default=0): cv.int_,
+                }
+            ),
+            cv.Optional(CONF_ACCELERATION_MODE): cv.enum(ACCELERATION_MODES),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(i2c.i2c_device_schema(0x69))
+)
